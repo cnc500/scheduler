0310aa9a4f05f91ad9cd7babea46fdb62d6996c0
Christian Carnevale <cncarnevale@gmail.com>
2022-04-10 15:07:44 -0400

    created textareas, timer and textarea color updated every second

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+$('#timer');
+
+function displayCureentDayTime() {
+var currentDateTime = moment().format("dddd, MMMM Do, YYYY  hh:mm:ss");
+console.log(currentDateTime);
+$('#currentDayTime').text(currentDateTime);
+}
+
+setInterval(displayCureentDayTime, 1000);
+
+function changeTextareaColor() {
+    $('textArea').each(function(){
+//    currentHour = currentDateTime."hh"
+    var currentHour = moment().hour();
+    console.log(currentHour);
+    var timeBlockHour = $(this).parent().attr('id');
+    console.log(timeBlockHour);
+    if (currentHour===parseInt(timeBlockHour)){
+        $(this).addClass("present");
+    } else if (currentHour>parseInt(timeBlockHour)){
+        $(this).addClass("past");
+    } else {
+        $(this).addClass("future");
+    }
+    })
+}
+
+setInterval(changeTextareaColor, 1000); 
+
+$('.saveBtn').on('click',function(){
+    
